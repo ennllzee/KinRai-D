e5824b7ec1daa9623c926d3441f4a34dbe2665c2
Pongpisut1999 <nattha_c@elearning.cmu.ac.th>
2020-04-05 15:21:24 +0700

    Updates

---
+++ precache-manifest.b231f88476c3daef582b0b0c25acaede.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c4696353516cc2cc9cf9a069412d65",
+    "url": "/KinRai-D/index.html"
+  },
+  {
+    "revision": "013b5680259869275540",
+    "url": "/KinRai-D/static/css/main.0d031e74.chunk.css"
+  },
+  {
+    "revision": "ca9e59591085828401c1",
+    "url": "/KinRai-D/static/js/2.6665392d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/KinRai-D/static/js/2.6665392d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013b5680259869275540",
+    "url": "/KinRai-D/static/js/main.a8f7c0fe.chunk.js"
+  },
+  {
+    "revision": "a80a0b10c17f48d45ddb",
+    "url": "/KinRai-D/static/js/runtime-main.3ac49518.js"
+  },
+  {
+    "revision": "be9103a0c5e7f85e1a316f91e6412c2e",
+    "url": "/KinRai-D/static/media/Logo1.be9103a0.png"
+  }
+]);
