a7d6dd80b1daad8b727340f17d74323dbddd892b
Pongpisut1999 <nattha_c@elearning.cmu.ac.th>
2020-04-10 11:31:03 +0700

    erroe เฉยย

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,9 @@ connection.once('open', () => {
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
-const registryRouter = require('./routes/registries');
 
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
-app.use('/registries', registryRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
